149b709dbe6d70449ed7edc7b6bcf141ca0f6772
1942049 <72592323+1942049@users.noreply.github.com>
2021-07-08 18:29:55 +0900

    Add files via upload

---
+++ htdocs/jobs.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>課題確認</title>
+               <link rel="stylesheet" href="jobs.css" >
+    </head>
+    <body>
+        <h1 style="background-color: rgb(130, 131, 130);">
+        <center>予定確認</center></h1>
+
+      <div class="jobs2-box">
+        <div class="jobs1-box"　
+           style="background-color: #70c6ffc2;">
+        <center>仕事</center>
+        </div>
+      </div>
+      <div class="hyou1">
+        <style>
+         section {
+	        overflow: scroll;
+ 	        margin: 10px auto;
+	        padding: 100px;
+	        width: 1000px;
+	        height: 200px;
+	        border: 2px solid #ccc;} 
+          p {	width: 5px;
+              }
+          figure { width: 30px;}
+        </style>
+         <section>
+	      <?php
+          
+          ?>
+         </section>
+         <div class="jobs3-box">
+           <div class="container1">
+             <a href="#" class="btn-border">前の画面へ戻る</a>
+           </div>
+            </div>
+            <div class="jobs4-box">
+           <div class="container2">
+             <a href="#" class="btn-border1"></a>
+            </div>
+            </div>
+   </body>
+</html>
